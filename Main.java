ef51518070c91d62843c45d12aa8f886fc11eb28
rickastley10 <148710838+rickastley10@users.noreply.github.com>
2025-07-23 18:33:03 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,7 +7,7 @@ public class Main {
         System.out.println("a TYPING SIMULATOR!!!!");
         int points = 0;
         Scanner scanner = new Scanner(System.in);
-        
+
         while (true) {
             System.out.println("\n\n\n type ANYTHING \n(remember to find all the letter comboes like abcd)\n\n");
             String anything = scanner.next();
@@ -16,32 +16,26 @@ public class Main {
             System.out.print("\n you have ");
             System.out.print(points);
             System.out.print(" points \n");
-            
+
             if (anything.equals("abcd")) {
                 System.out.print("WOW, a whole abcd combo!!!");
                 System.out.print("+10 points");
                 points = points + 10;
-            }
-            else if (anything.equals("wxyz")) {
+            } else if (anything.equals("wxyz")) {
                 System.out.print("WOW, a whole wxyz combo!!!");
                 System.out.print("+10 points");
                 points = points + 10;
-            }
-            else if (anything.equals("qwertyuiop")) {
+            } else if (anything.equals("qwertyuiop")) {
                 System.out.print("WOW, a whole qwertyuiop combo!!!");
                 System.out.print("+30 points");
                 points = points + 30;
-            }
-            else if (anything.equals("abcd")) {
-                System.out.print("WOW, a whole abcd combo!!!");
-                System.out.print("+10 points");
-                points = points + 10;
-            }
-            else if (anything.equals("wasd")) {
+            } else if (anything.equals("wasd")) {
                 System.out.print("\nWOW, a whole wasd combo!!!");
                 System.out.print("+10 points\n");
                 points = points + 10;
-            else iff (anything.equals("wasd")) {
+            }
+            else if (anything.equals("exit"))
+            {
                 System.exit(0);
             }
         }
